c26d3e07c9402c7e604ca8804c16bf60e1b94f34
Daniil Yarockii <yaroc.d@gmail.com>
2022-01-14 16:57:30 +0300

    modal updated

--- supervisely/instance_segmentation/serve/src/pretrained_models.py
+++ supervisely/instance_segmentation/serve/src/pretrained_models.py
@@ -23,7 +23,7 @@ def get_pretrained_models():
                 "model_id": 137849551
             },
             {
-                "config": "../../../configs/new_baselines/mask_rcnn_R_50_FPN_100ep_LSJ.py",
+                "config": "new_baselines/mask_rcnn_R_50_FPN_100ep_LSJ.py",
                 "weightsUrl": "https://dl.fbaipublicfiles.com/detectron2/new_baselines/mask_rcnn_R_50_FPN_100ep_LSJ/42047764/model_final_bb69de.pkl",
                 "model": "R50-FPN (100)",
                 "train_time": 0.376,
@@ -33,7 +33,7 @@ def get_pretrained_models():
                 "model_id": 42047764
             },
             {
-                "config": "COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_1x.yaml",
+                "config": "new_baselines/mask_rcnn_R_50_FPN_400ep_LSJ.py",
                 "weightsUrl": "https://dl.fbaipublicfiles.com/detectron2/new_baselines/mask_rcnn_R_50_FPN_400ep_LSJ/42019571/model_final_14d201.pkl",
                 "model": "R50-FPN (400)",
                 "train_time": 0.376,
@@ -43,7 +43,7 @@ def get_pretrained_models():
                 "model_id": 42019571
             },
             {
-                "config": "COCO-InstanceSegmentation/mask_rcnn_R_101_FPN_3x.yaml",
+                "config": "new_baselines/mask_rcnn_R_101_FPN_100ep_LSJ.py",
                 "weightsUrl": "https://dl.fbaipublicfiles.com/detectron2/new_baselines/mask_rcnn_R_101_FPN_100ep_LSJ/42025812/model_final_4f7b58.pkl",
                 "model": "R101-FPN (100)",
                 "train_time": 0.376,
@@ -53,7 +53,7 @@ def get_pretrained_models():
                 "model_id": 42025812
             },
             {
-                "config": "COCO-InstanceSegmentation/mask_rcnn_R_101_FPN_3x.yaml",
+                "config": "new_baselines/mask_rcnn_R_101_FPN_400ep_LSJ.py",
                 "weightsUrl": "https://dl.fbaipublicfiles.com/detectron2/new_baselines/mask_rcnn_R_101_FPN_400ep_LSJ/42073830/model_final_f96b26.pkl",
                 "model": "R101-FPN (400)",
                 "train_time": 0.376,
@@ -63,7 +63,7 @@ def get_pretrained_models():
                 "model_id": 42073830
             },
             {
-                "config": "COCO-InstanceSegmentation/mask_rcnn_regnetx_4gf_dds_fpn_1x.py",
+                "config": "new_baselines/mask_rcnn_regnetx_4gf_dds_FPN_100ep_LSJ.py",
                 "weightsUrl": "https://dl.fbaipublicfiles.com/detectron2/new_baselines/mask_rcnn_regnetx_4gf_dds_FPN_100ep_LSJ/42047771/model_final_b7fbab.pkl",
                 "model": "regnetx_4gf_dds_FPN (100)",
                 "train_time": 0.474,
@@ -73,7 +73,7 @@ def get_pretrained_models():
                 "model_id": 42047771
             },
             {
-                "config": "mask_rcnn_regnetx_4gf_dds_fpn_1x.yaml",
+                "config": "new_baselines/mask_rcnn_regnetx_4gf_dds_FPN_400ep_LSJ.py",
                 "weightsUrl": "https://dl.fbaipublicfiles.com/detectron2/new_baselines/mask_rcnn_regnetx_4gf_dds_FPN_400ep_LSJ/42025447/model_final_f1362d.pkl",
                 "model": "regnetx_4gf_dds_FPN (400)",
                 "train_time": 0.474,
@@ -83,7 +83,7 @@ def get_pretrained_models():
                 "model_id": 42025447
             },
             {
-                "config": "mask_rcnn_regnety_4gf_dds_fpn_1x.yaml",
+                "config": "new_baselines/mask_rcnn_regnety_4gf_dds_FPN_100ep_LSJ.py",
                 "weightsUrl": "https://dl.fbaipublicfiles.com/detectron2/new_baselines/mask_rcnn_regnety_4gf_dds_FPN_100ep_LSJ/42047784/model_final_6ba57e.pkl",
                 "model": "regnety_4gf_dds_FPN (100)",
                 "train_time": 0.487,
@@ -93,7 +93,7 @@ def get_pretrained_models():
                 "model_id": 42047784
             },
             {
-                "config": "mask_rcnn_regnety_4gf_dds_fpn_1x.yaml",
+                "config": "new_baselines/mask_rcnn_regnety_4gf_dds_FPN_400ep_LSJ.py",
                 "weightsUrl": "https://dl.fbaipublicfiles.com/detectron2/new_baselines/mask_rcnn_regnety_4gf_dds_FPN_400ep_LSJ/42045954/model_final_ef3a80.pkl",
                 "model": "regnety_4gf_dds_FPN (400)",
                 "train_time": 0.487,
@@ -107,7 +107,7 @@ def get_pretrained_models():
 
         "LVIS": [
             {
-                "config": "mask_rcnn_R_50_FPN_1x.yaml",
+                "config": "LVISv0.5-InstanceSegmentation/mask_rcnn_R_50_FPN_1x.yaml",
                 "weightsUrl": "https://dl.fbaipublicfiles.com/detectron2/LVISv0.5-InstanceSegmentation/mask_rcnn_R_50_FPN_1x/144219072/model_final_571f7c.pkl",
                 "model": "R50-FPN",
                 "train_time": 0.292,
@@ -117,7 +117,7 @@ def get_pretrained_models():
                 "model_id": 144219072
             },
             {
-                "config": "mask_rcnn_R_101_FPN_1x.yaml",
+                "config": "LVISv0.5-InstanceSegmentation/mask_rcnn_R_101_FPN_1x.yaml",
                 "weightsUrl": "https://dl.fbaipublicfiles.com/detectron2/LVISv0.5-InstanceSegmentation/mask_rcnn_R_101_FPN_1x/144219035/model_final_824ab5.pkl",
                 "model": "R101-FPN",
                 "train_time": 0.371,
@@ -127,7 +127,7 @@ def get_pretrained_models():
                 "model_id": 144219035
             },
             {
-                "config": "mask_rcnn_X_101_32x8d_FPN_1x.yaml",
+                "config": "LVISv0.5-InstanceSegmentation/mask_rcnn_X_101_32x8d_FPN_1x.yaml",
                 "weightsUrl": "https://dl.fbaipublicfiles.com/detectron2/LVISv0.5-InstanceSegmentation/mask_rcnn_X_101_32x8d_FPN_1x/144219108/model_final_5e3439.pkl",
                 "model": "X101-FPN",
                 "train_time": 0.712,
@@ -140,7 +140,7 @@ def get_pretrained_models():
 
         "Cityscapes": [
             {
-                "config": "mask_rcnn_R_50_FPN.yaml",
+                "config": "Cityscapes/mask_rcnn_R_50_FPN.yaml",
                 "weightsUrl": "https://dl.fbaipublicfiles.com/detectron2/Cityscapes/mask_rcnn_R_50_FPN/142423278/model_final_af9cf5.pkl",
                 "model": "R50-FPN",
                 "train_time": 0.240,
@@ -153,7 +153,7 @@ def get_pretrained_models():
 
         "Others": [
             {
-                "config": "mask_rcnn_R_50_FPN_3x_dconv_c3-c5.yaml",
+                "config": "Misc/mask_rcnn_R_50_FPN_3x_dconv_c3-c5.yaml",
                 "weightsUrl": "https://dl.fbaipublicfiles.com/detectron2/Misc/mask_rcnn_R_50_FPN_3x_dconv_c3-c5/144998336/model_final_821d0b.pkl",
                 "model": "Deformable Conv (3x)",
                 "train_time": 0.349,
@@ -163,7 +163,7 @@ def get_pretrained_models():
                 "model_id": 144998336
             },
             {
-                "config": "cascade_mask_rcnn_R_50_FPN_3x.yaml",
+                "config": "Misc/cascade_mask_rcnn_R_50_FPN_3x.yaml",
                 "weightsUrl": "https://dl.fbaipublicfiles.com/detectron2/Misc/cascade_mask_rcnn_R_50_FPN_3x/144998488/model_final_480dd8.pkl",
                 "model": "Cascade R-CNN (3x)",
                 "train_time": 0.328,
@@ -173,7 +173,7 @@ def get_pretrained_models():
                 "model_id": 144998488
             },
             {
-                "config": "mask_rcnn_R_50_FPN_3x_gn.yaml",
+                "config": "Misc/mask_rcnn_R_50_FPN_3x_gn.yaml",
                 "weightsUrl": "https://dl.fbaipublicfiles.com/detectron2/Misc/mask_rcnn_R_50_FPN_3x_gn/138602888/model_final_dc5d9e.pkl",
                 "model": "GN (3x)",
                 "train_time": 0.309,
@@ -183,27 +183,7 @@ def get_pretrained_models():
                 "model_id": 138602888
             },
             {
-                "config": "mask_rcnn_R_50_FPN_3x_syncbn.yaml",
-                "weightsUrl": "https://dl.fbaipublicfiles.com/detectron2/Misc/mask_rcnn_R_50_FPN_3x_syncbn/169527823/model_final_3b3c51.pkl",
-                "model": "SyncBN (3x)",
-                "train_time": 0.345,
-                "inference_time": 0.053,
-                "box": 41.9,
-                "mask": 37.8,
-                "model_id": 169527823
-            },
-            {
-                "config": "panoptic_fpn_R_101_dconv_cascade_gn_3x.yaml",
-                "weightsUrl": "https://dl.fbaipublicfiles.com/detectron2/Misc/panoptic_fpn_R_101_dconv_cascade_gn_3x/139797668/model_final_be35db.pkl",
-                "model": "Panoptic FPN R101",
-                "train_time": "-",
-                "inference_time": 0.098,
-                "box": 47.4,
-                "mask": 41.3,
-                "model_id": 139797668
-            },
-            {
-                "config": "cascade_mask_rcnn_X_152_32x8d_FPN_IN5k_gn_dconv.yaml",
+                "config": "Misc/cascade_mask_rcnn_X_152_32x8d_FPN_IN5k_gn_dconv.yaml",
                 "weightsUrl": "https://dl.fbaipublicfiles.com/detectron2/Misc/cascade_mask_rcnn_X_152_32x8d_FPN_IN5k_gn_dconv/18131413/model_0039999_e76410.pkl",
                 "model": "Mask R-CNN X152",
                 "train_time": "-",
@@ -215,7 +195,6 @@ def get_pretrained_models():
 
 
         ]
-
     }
 
 
