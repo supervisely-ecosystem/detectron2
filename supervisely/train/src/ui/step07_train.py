093e3abcec4062e7e5567eb0dfd81db2e64dd163
cxnt <pavelbartsits@gmail.com>
2025-08-12 15:36:42 +0000

    Refactor error logging in experiment creation to improve readability and maintain consistency in logging format.

--- supervisely/train/src/ui/step07_train.py
+++ supervisely/train/src/ui/step07_train.py
@@ -628,9 +628,7 @@ def train(api: sly.Api, task_id, context, state, app_logger):
                 model_name, remote_dir, report_id, eval_metrics, primary_metric_name
             )
         except Exception as e:
-            sly.logger.error(
-                f"Couldn't create experiment, this training session will not appear in the experiments table. Error: {e}"
-            )
+            sly.logger.error(f"Couldn't create experiment, this training session will not appear in the experiments table. Error: {e}")
 
 
     except Exception as e:
